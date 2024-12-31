b151a1591db2c69f412f9ac10961fc5a8702c451
naveed-a-sattar <naveed@techeguys.com>
2024-12-31 08:19:21 +0500

    first commit

---
+++ setup-hosts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+# remove ubuntu-bionic entry
+sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.168.56.1  kubeControlPlane1
+192.168.56.11  kubenode1
+192.168.56.12  kubenode2
+EOF
